4bf873653698c1ecae22d410d2907528037c5ee8
Damiano Gianotti <damiano.gianotti@edu.unito.it>
2021-04-22 18:47:28 +0200

    - Added Demaria Annotation - Added Radicioni's function necessary for the assignment

--- DisambiguateFN/utils.py
+++ DisambiguateFN/utils.py
@@ -1,3 +1,9 @@
+import hashlib
+from random import randint
+from random import seed
+
+from nltk.corpus import framenet as fn
+
 
 def read_correct_synsets(path):
     """
@@ -16,3 +22,34 @@ def read_correct_synsets(path):
             if "#" not in syn:
                 correct_synset.append(syn)
     return correct_synset
+
+
+"""
+    The functions below these lines were made by professor Radicioni
+"""
+
+
+def get_frame_set_for_student(surname, list_len=5):
+    nof_frames = len(fn.frames())
+    base_idx = (abs(int(hashlib.sha512(surname.encode('utf-8')).hexdigest(), 16)) % nof_frames)
+    print('\nstudent: ' + surname)
+    framenet_IDs = get_frames_ids()
+    i = 0
+    offset = 0
+    seed(1)
+    while i < list_len:
+        fID = framenet_IDs[(base_idx+offset)%nof_frames]
+        f = fn.frame(fID)
+        fNAME = f.name
+        print('\tID: {a:4d}\tframe: {framename}'.format(a=fID, framename=fNAME))
+        offset = randint(0, nof_frames)
+        i += 1
+
+
+def print_frames_with_ids():
+    for x in fn.frames():
+        print('{}\t{}'.format(x.ID, x.name))
+
+
+def get_frames_ids():
+    return [f.ID for f in fn.frames()]
--- esercizio2.py
+++ esercizio2.py
@@ -1,12 +1,22 @@
 from pathlib import Path
 
-from DisambiguateFN.utils import read_correct_synsets
+from DisambiguateFN.utils import read_correct_synsets, get_frame_set_for_student
 
+surnames = ['gianotti', 'Demaria']
 """
 In this file, we executes Task 2 (FrameNet Disambiguation)
 """
 
 if __name__ == "__main__":
-    surname = ['gianotti']
-    correctSynsetsPath = Path('.') / 'datasets' / 'AnnotationsFN' / (surname[0] + '.txt')
-    test = read_correct_synsets(correctSynsetsPath)
+    correct_synsets_path = Path('.') / 'datasets' / 'AnnotationsFN'
+    output_path = Path('.') / 'output'
+
+    with open(output_path / 'results.csv', "w", encoding="utf-8") as out:
+        for surname in surnames:
+            fids = get_frame_set_for_student(surname)
+
+            surname_path = correct_synsets_path / (surname + '.txt')
+            read_correct_synsets(surname_path)
+
+
+
