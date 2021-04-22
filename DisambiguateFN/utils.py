6fcc64ebce252b734aa78c446ea06fe33230c71a
Damiano Gianotti <damiano.gianotti@edu.unito.it>
2021-04-22 18:56:27 +0200

    - retrive the Id of frames - setup for future work

--- DisambiguateFN/utils.py
+++ DisambiguateFN/utils.py
@@ -30,6 +30,7 @@ def read_correct_synsets(path):
 
 
 def get_frame_set_for_student(surname, list_len=5):
+    ids_list = []
     nof_frames = len(fn.frames())
     base_idx = (abs(int(hashlib.sha512(surname.encode('utf-8')).hexdigest(), 16)) % nof_frames)
     print('\nstudent: ' + surname)
@@ -40,11 +41,12 @@ def get_frame_set_for_student(surname, list_len=5):
     while i < list_len:
         fID = framenet_IDs[(base_idx+offset)%nof_frames]
         f = fn.frame(fID)
+        ids_list.append(fID)
         fNAME = f.name
         print('\tID: {a:4d}\tframe: {framename}'.format(a=fID, framename=fNAME))
         offset = randint(0, nof_frames)
         i += 1
-
+    return ids_list
 
 def print_frames_with_ids():
     for x in fn.frames():
--- esercizio2.py
+++ esercizio2.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from nltk.corpus import framenet as fn
+
 from DisambiguateFN.utils import read_correct_synsets, get_frame_set_for_student
 
 surnames = ['gianotti', 'Demaria']
@@ -12,11 +14,17 @@ if __name__ == "__main__":
     output_path = Path('.') / 'output'
 
     with open(output_path / 'results.csv', "w", encoding="utf-8") as out:
+
+        print("Assigning Synsets...")
+
         for surname in surnames:
-            fids = get_frame_set_for_student(surname)
+            frame_ids = get_frame_set_for_student(surname)
 
             surname_path = correct_synsets_path / (surname + '.txt')
             read_correct_synsets(surname_path)
+            for fID in frame_ids:
+                frame = fn.frame_by_id(fID)
 
+                # calculate context ctx_f and ctx_w
 
 
