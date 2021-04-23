d1363365beddca87014c538c04207a259bcc7d8c
Damiano <37343600+d3miangianoz@users.noreply.github.com>
2021-04-23 18:27:09 +0200

    + Added skeletons of :     - populate contexts     - bag of words

--- esercizio2.py
+++ esercizio2.py
@@ -31,6 +31,30 @@ def get_main_clause(frame_name):
             return elem[0]
 
 
+def populate_contexts(f, mode: str):
+    context_w = []  # the Framenet context
+    context_s = {}  # the Wordnet context
+
+    if mode == "Frame name":
+        main_clause = get_main_clause(f.name)
+        context_w = [main_clause, f.definition]
+
+        context_s = {"test": main_clause}
+
+    elif mode == "FEs":
+        # Populating ctx_w for FEs
+        pass
+    elif mode == "LUs":
+        # Populating ctx_w for LUs
+        pass
+
+    return context_w, context_s
+
+
+def bag_of_words(ctx_fn, ctx_wn):
+    return True
+
+
 if __name__ == "__main__":
     correct_synsets_path = Path('.') / 'datasets' / 'AnnotationsFN'
     output_path = Path('.') / 'output'
@@ -46,8 +70,10 @@ if __name__ == "__main__":
             read_correct_synsets(surname_path)
             for fID in frame_ids:
                 frame = fn.frame_by_id(fID)
-                # calculate context ctx_f and ctx_w
+                # calculate context of FN: ctx_w and WN: ctx_s
+                ctx_w, ctx_s = populate_contexts(frame, "Frame name")
+                sense_name = bag_of_words(ctx_fn=ctx_w, ctx_wn=ctx_s)
 
-                print(get_main_clause(frame.name))
+            out.write("Frame name, {0}, Wordnet Synset, {1}\n".format(frame.name, sense_name))
 
     evaluate_performance()
