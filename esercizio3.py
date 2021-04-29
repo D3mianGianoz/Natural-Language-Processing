57f0982c70227c4c83c5a72567d06c526949b739
skaterobi88 <73557317+skaterobi88@users.noreply.github.com>
2021-04-29 15:57:44 +0200

    Update esercizio3.py
    Ho inserito la vecchia funzione di bag of words e la funzione get_Nasari_vectors

--- esercizio3.py
+++ esercizio3.py
@@ -2,6 +2,44 @@ from pathlib import Path
 
 from Summarize.utils import read_from_file
 
+def bag_of_word(sent):
+    """Auxiliary function for the Lesk algorithm. Transforms the given sentence
+    according to the bag of words approach, apply lemmatization, stop words
+    and punctuation removal.
+    Params:
+        sent: sentence
+    Returns:
+        bag of words
+    """
+
+    stop_words = set(stopwords.words('english'))
+    punctuation = {',', ';', '(', ')', '{', '}', ':', '?', '!'}
+    # Returns the input word unchanged if it cannot be found in WordNet.
+    wnl = nltk.WordNetLemmatizer()
+    # Return a tokenized copy of text, using NLTK’s recommended word tokenizer (Treebank + PunkSentence)
+    tokens = nltk.word_tokenize(sent)
+    tokens = list(filter(lambda x: x not in stop_words and x not in punctuation, tokens))
+    return set(wnl.lemmatize(t) for t in tokens)
+
+def get_Nasari_vectors(titolo, Nasari_vector):
+    """Given a sentence, it creates a bag of words of this sentence
+    and return the Nasari vectors for the words in the sentence
+    Params:
+        title
+        Nazari vector
+    Returns:
+        Nazari vectors of words in title
+    """
+    
+    bag = list(bag_of_word(titolo))
+    print(bag)
+    
+    for word in bag:
+        if word in str(Nasari_vector.keys()).lower():
+            print(word)
+            vettore = Nasari_vector[str(word).title()]
+            print(vettore)
+
 if __name__ == "__main__":
     nasari_path = Path('.') / 'datasets' / 'NASARI_vectors' / 'dd-small-nasari-15.txt'
 
