119e6b04e25f584bc0b2d793fe75fbc900a8fcfe
skaterobi88 <73557317+skaterobi88@users.noreply.github.com>
2021-05-07 15:18:59 +0200

    Update esercizio4.py
    ho messo un metodo che fa il parsing del file tsv e lo converte in un comodo dizionario con chiave il babel vector ed elementi i 300 numeri del vettore

--- esercizio4.py
+++ esercizio4.py
@@ -1,4 +1,31 @@
 from pathlib import Path
+import re
+
+def parse_nasari_dictionary(path):
+    """It parse the Nasari input file, and it converts into a more convenient
+    Python dictionary.
+    Returns:
+         a dictionary representing the Nasari input file.
+    Format: {word: {term:score}}
+    """
+
+    nasari_dict = {}
+    
+    
+    with open(path, 'r', encoding="utf8") as file:
+        for line in file.readlines():
+            splits = line.split("\t")
+            vector_nasari = []
+            
+            for term in splits[1:]:
+                vector_nasari.append(re.sub('\n', '', term))
+                
+            for key in splits[:1]:
+                new_key = key.split("n__")
+
+            nasari_dict[new_key[0]] = vector_nasari
+
+    return nasari_dict
 
 from Radicioni.Annotation.parsing import read_from_annotation
 
